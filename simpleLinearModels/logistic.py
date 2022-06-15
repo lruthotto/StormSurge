05187f082ab16e33d08f17ea51c0b7605261be93
loganpknudsen <logantheginjaninja@gmail.com>
2022-06-15 14:52:41 -0400

    LK: Added to Logistic
    Created function to solve for logistic differential equation

---
+++ simpleLinearModels/logistic.py
@@ -0,0 +1,13 @@
+#Question 1:
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as sc
+def logist_cont(y,t,r,k):
+    return r*y*(1-y/k)
+r=1
+k=2
+t=np.linspace(0,10,10000)
+y0=0.2
+sol=sc.odeint(logist_cont, y0, t, args=(r, k))
+plt.plot(t,sol)
+plt.show()

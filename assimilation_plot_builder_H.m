cf98a3afe05a8b41f944d11188b9860b79e10ff5
loganpknudsen <logantheginjaninja@gmail.com>
2022-07-13 19:21:16 -0400

    LK

--- assimilation_plot_builder_H.m
+++ assimilation_plot_builder_H.m
@@ -1,6 +1,6 @@
-T = readtable("data_matrix_10_analysis/smbt11950smbtf2300smb00.324smb10.162smbf-0.015sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
-T2 = readtable("data_matrix_10/smbt11950smbtf2300smb00.324smb10.162smbf-0.015sillmin415sillmax425sillslope0.01hnd2.35lnd4.6bx-0.001.csv");
-OBS = readtable("data_matrix_10_analysis/obs_smbt11950smbtf2300smb00.324smb10.162smbf-0.015sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+T = readtable("data_single_cases_analysis/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+T2 = readtable("data_single_cases/truth_simulation/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+OBS = readtable("data_single_cases_analysis/obs_smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
 t0 = table2array(T(:,"t"));
 t1=table2array(T(2:end,"t"));
 t2=table2array(OBS(:,"t"));
@@ -17,7 +17,7 @@ title("t vs. H(t)")
 saveas(gcf, "H(t)truth.png")
 plot(t2,table2array(OBS(:,"H")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","H(t): Observations")
 legend
-saveas(gcf, "H(t)truth_obs.png")
+saveas(gcf, "data_single_cases_analysis/H(t)truth_obs.png")
 hold off
 figure(2)
 plot(t0,table2array(T(:,"H")),"r","LineWidth",4)
@@ -33,8 +33,8 @@ xlabel("t")
 ylim([1.5 2.5])
 ylabel("H(t)")
 legend("H(t): Truth","H(t): Different Initial Conditions", "H(t): Observations", "H(t): Ensemble")
-saveas(gcf, "H(t)truth_and_ensemble_obs.png")
+saveas(gcf, "data_single_cases_analysis/H(t)truth_and_ensemble_obs.png")
 plot(t0,table2array(T(:,"aH")),"k","LineWidth",4,"DisplayName","H(t): Analysis")
 legend
 hold off
-saveas(gcf, "H(t)truth_ensemble_and_analysis_obs.png")
+saveas(gcf, "data_single_cases_analysis/H(t)truth_ensemble_and_analysis_obs.png")
--- assimilation_plot_builder_L.m
+++ assimilation_plot_builder_L.m
@@ -1,29 +1,40 @@
-T = readtable("data_matrix_10_analysis/smbt11950smbtf2300smb00.324smb10.162smbf-0.015sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+T = readtable("data_single_cases_analysis/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+T2 = readtable("data_single_cases/truth_simulation/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+OBS = readtable("data_single_cases_analysis/obs_smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
 t0 = table2array(T(:,"t"));
 t1=table2array(T(2:end,"t"));
+t2=table2array(OBS(:,"t"));
 figure(1)
 plot(t0,table2array(T(:,"L")),"r","LineWidth",4,"DisplayName","L(t): Truth")
+hold on
+plot(t0,table2array(T2(:,"L")),"b","LineWidth",4,"DisplayName","L(t): Different Initial Conditions")
 xlim([0 2300])
 xlabel("t")
-ylim([4.5 6.5])
+ylim([3.6 4.6])
 ylabel("L(t)")
-legend("Location","northwest")
+legend
 title("t vs. L(t)")
 saveas(gcf, "L(t)truth.png")
+plot(t2,table2array(OBS(:,"L")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","L(t): Observations")
+legend
+saveas(gcf, "data_single_cases_analysis/L(t)truth_obs.png")
+hold off
 figure(2)
 plot(t0,table2array(T(:,"L")),"r","LineWidth",4)
 title("t vs. L(t)")
 hold on
+plot(t0,table2array(T2(:,"L")),"b","LineWidth",4)
+plot(t2,table2array(OBS(:,"L")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","L(t): Observations")
 for i=1:10
     plot(t1,table2array(T(2:end,"x"+int2str(i)+"L")),"Color",[0, 0.6, 0],"LineWidth",2,"LineStyle","--","DisplayName","L(t): Ensemble")
 end
 xlim([0 2300])
 xlabel("t")
-ylim([4.5 6.5])
+ylim([3.6 4.6])
 ylabel("L(t)")
-legend("L(t): Truth","L(t): Ensemble","Location","northwest")
-saveas(gcf, "L(t)truth_and_ensemble.png")
+legend("L(t): Truth","L(t): Different Initial Conditions", "L(t): Observations", "L(t): Ensemble")
+saveas(gcf, "data_single_cases_analysis/L(t)truth_and_ensemble_obs.png")
 plot(t0,table2array(T(:,"aL")),"k","LineWidth",4,"DisplayName","L(t): Analysis")
-legend("Location","northwest")
+legend
 hold off
-saveas(gcf, "L(t)truth_ensemble_and_analysis.png")
+saveas(gcf, "data_single_cases_analysis/L(t)truth_ensemble_and_analysis_obs.png")

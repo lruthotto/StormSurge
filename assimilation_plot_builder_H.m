127ca9577ff680a6c295f6e17cd3d9613732f60b
loganpknudsen <logantheginjaninja@gmail.com>
2022-07-15 16:57:23 -0400

    LK

--- assimilation_plot_builder_H.m
+++ assimilation_plot_builder_H.m
@@ -1,6 +1,6 @@
-T = readtable("data_single_cases_analysis/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001_fixed_obs.csv");
+T = readtable("data_single_cases_analysis/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
 T2 = readtable("data_single_cases/truth_simulation/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
-OBS = readtable("data_single_cases_analysis/obs_smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001_fixed_obs.csv");
+OBS = readtable("data_single_cases_analysis/obs_smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
 t0 = table2array(T(:,"t"));
 t1=table2array(T(2:end,"t"));
 t2=table2array(OBS(:,"t"));
@@ -14,10 +14,10 @@ ylim([1.5 2.5])
 ylabel("H(t)")
 legend
 title("t vs. H(t)")
-saveas(gcf, "H(t)truth_fixed_obs.png")
+saveas(gcf, "H(t)truth.png")
 plot(t2,table2array(OBS(:,"H")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","H(t): Observations")
 legend
-saveas(gcf, "data_single_cases_analysis/H(t)truth_obs_fixed_obs.png")
+saveas(gcf, "data_single_cases_analysis/H(t)truth_obs.png")
 hold off
 figure(2)
 plot(t0,table2array(T(:,"H")),"r","LineWidth",4)
@@ -33,11 +33,11 @@ xlabel("t")
 ylim([1.5 2.5])
 ylabel("H(t)")
 legend("H(t): Truth","H(t): Different Initial Conditions", "H(t): Observations", "H(t): Ensemble")
-saveas(gcf, "data_single_cases_analysis/H(t)truth_and_ensemble_obs_fixed_obs.png")
+saveas(gcf, "data_single_cases_analysis/H(t)truth_and_ensemble_obs.png")
 plot(t0,table2array(T(:,"aH")),"k","LineWidth",4,"DisplayName","H(t): Analysis")
 legend
 hold off
-saveas(gcf, "data_single_cases_analysis/H(t)truth_ensemble_and_analysis_obs_fixed_obs.png")
+saveas(gcf, "data_single_cases_analysis/H(t)truth_ensemble_and_analysis_obs.png")
 figure(3)
 plot(t0,table2array(T2(:,"H")),"b","LineWidth",4)
 title("t vs. H(t)")
@@ -50,6 +50,6 @@ ylabel("H(t)")
 plot(t0,table2array(T(:,"aH")),"k","LineWidth",4,"DisplayName","H(t): Analysis")
 legend
 hold off
-saveas(gcf, "data_single_cases_analysis/H(t)truth_and_analysis_obs_fixed_obs.png")
+saveas(gcf, "data_single_cases_analysis/H(t)truth_and_analysis_obs.png")
 
 

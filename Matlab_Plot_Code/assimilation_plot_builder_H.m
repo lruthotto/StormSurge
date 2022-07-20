e4d8066e5b396e10e4256b682f06e2a4415fb708
loganpknudsen <logantheginjaninja@gmail.com>
2022-07-20 15:34:11 -0400

    LK

--- Matlab_Plot_Code/assimilation_plot_builder_H.m
+++ Matlab_Plot_Code/assimilation_plot_builder_H.m
@@ -1,6 +1,6 @@
-T = readtable("data_single_cases_analysis/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
-T2 = readtable("data_single_cases/truth_simulation/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
-OBS = readtable("data_single_cases_analysis/obs_smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+T = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
+T2 = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases/truth_simulation/smbt11950smbtf2300smb00.35smb10.15smbf0.0sillmin415sillmax425sillslope0.001hnd2.3lnd4.6bx-0.001.csv");
+OBS = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/obs_smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
 t0 = table2array(T(:,"t"));
 t1=table2array(T(2:end,"t"));
 t2=table2array(OBS(:,"t"));
@@ -14,10 +14,10 @@ ylim([1.5 2.5])
 ylabel("H(t)")
 legend
 title("t vs. H(t)")
-saveas(gcf, "H(t)truth.png")
+%saveas(gcf, "H(t)truth.png")
 plot(t2,table2array(OBS(:,"H")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","H(t): Observations")
 legend
-saveas(gcf, "data_single_cases_analysis/H(t)truth_obs.png")
+%saveas(gcf, "data_single_cases_analysis/H(t)truth_obs.png")
 hold off
 figure(2)
 plot(t0,table2array(T(:,"H")),"r","LineWidth",4)
@@ -33,15 +33,16 @@ xlabel("t")
 ylim([1.5 2.5])
 ylabel("H(t)")
 legend("H(t): Truth","H(t): Different Initial Conditions", "H(t): Observations", "H(t): Ensemble")
-saveas(gcf, "data_single_cases_analysis/H(t)truth_and_ensemble_obs.png")
+%saveas(gcf, "data_single_cases_analysis/H(t)truth_and_ensemble_obs.png")
 plot(t0,table2array(T(:,"aH")),"k","LineWidth",4,"DisplayName","H(t): Analysis")
 legend
 hold off
-saveas(gcf, "data_single_cases_analysis/H(t)truth_ensemble_and_analysis_obs.png")
+%saveas(gcf, "data_single_cases_analysis/H(t)truth_ensemble_and_analysis_obs.png")
 figure(3)
-plot(t0,table2array(T2(:,"H")),"b","LineWidth",4)
-title("t vs. H(t)")
+plot(t0,table2array(T(:,"H")),"r","LineWidth",4,"DisplayName","H(t): Truth")
 hold on
+title("t vs. H(t)")
+plot(t0,table2array(T2(:,"H")),"b","LineWidth",4,"DisplayName","H(t): Differential Inital Condition")
 plot(t2,table2array(OBS(:,"H")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","L(t): Observations")
 xlim([0 2300])
 xlabel("t")
@@ -50,6 +51,4 @@ ylabel("H(t)")
 plot(t0,table2array(T(:,"aH")),"k","LineWidth",4,"DisplayName","H(t): Analysis")
 legend
 hold off
-saveas(gcf, "data_single_cases_analysis/H(t)truth_and_analysis_obs.png")
-
-
+%saveas(gcf, "data_single_cases_analysis/H(t)truth_and_analysis_obs.png")

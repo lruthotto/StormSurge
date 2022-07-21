94796b66b96c957402c989176ad6d6362bcb3972
loganpknudsen <logantheginjaninja@gmail.com>
2022-07-21 18:03:01 -0400

    LK

--- Matlab_Plot_Code/assimilation_plot_builder_H.m
+++ Matlab_Plot_Code/assimilation_plot_builder_H.m
@@ -1,5 +1,5 @@
-T = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
-T2 = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases/truth_simulation/smbt11950smbtf2300smb00.35smb10.15smbf0.0sillmin415sillmax425sillslope0.001hnd2.3lnd4.6bx-0.001.csv");
+T = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/smbt11950smbtf2300smb00.35smb10.15smbf0.0sillmin415sillmax425sillslope0.0018hnd2.3lnd4.6bx-0.001.csv");
+T2 = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases/truth_simulation/smbt11950smbtf2300smb00.35smb10.15smbf0.0sillmin415sillmax425sillslope0.008hnd2.3lnd4.6bx-0.001.csv");
 OBS = readtable("/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/obs_smbt11950smbtf2300smb00.3smb10.15smbf0.0sillmin415sillmax425sillslope0.01hnd2.18lnd4.44bx-0.001.csv");
 t0 = table2array(T(:,"t"));
 t1=table2array(T(2:end,"t"));
@@ -14,7 +14,7 @@ ylim([1.5 2.5])
 ylabel("H(t)")
 legend
 title("t vs. H(t)")
-saveas(gcf, "/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/H(t)truth.png")
+saveas(gcf, "/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/projections/H(t)truth.png")
 plot(t2,table2array(OBS(:,"H")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","H(t): Observations")
 legend
 saveas(gcf, "/Users/loganknudsen/Documents/GitHub/StormSurge/Experiment_Data/data_single_cases_analysis/H(t)truth_obs.png")
@@ -43,7 +43,7 @@ plot(t0,table2array(T(:,"H")),"r","LineWidth",3,"DisplayName","H(t): Truth")
 hold on
 title("t vs. H(t)")
 plot(t0,table2array(T2(:,"H")),"b","LineWidth",3,"DisplayName","H(t): Differential Inital Condition")
-plot(t2,table2array(OBS(:,"H")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","L(t): Observations")
+plot(t2,table2array(OBS(:,"H")),"b","LineStyle", "none", "Marker", "o", "MarkerEdgeColor", [0 0 0],"MarkerFaceColor", [0.9290 0.6940 0.1250], "DisplayName","H(t): Observations")
 xlim([0 2300])
 xlabel("t")
 ylim([1.5 2.5])
